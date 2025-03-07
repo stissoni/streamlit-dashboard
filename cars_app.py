20a0cc60f87c0bf4105d694286d0a7cd8fca8f82
Santiago Tissoni <santi.tissoni@gmail.com>
2025-03-07 13:16:52 -0300

    Added Mondeo and Cruze

--- cars_app.py
+++ cars_app.py
@@ -8,6 +8,8 @@ a1_page = st.Page("a1.py")
 a3_page = st.Page("a3.py")
 focus_page = st.Page("focus.py")
 compass_page = st.Page("compass.py")
+cruze_page = st.Page("cruze.py")
+mondeo_page = st.Page("mondeo.py")
 
 pg = st.navigation(
     [
@@ -18,6 +20,8 @@ pg = st.navigation(
         a1_page,
         a3_page,
         focus_page,
+        mondeo_page,
+        cruze_page,
     ]
 )
 
---
+++ cruze.py
@@ -0,0 +1,6 @@
+from page_factory.page_factory import PageFactory
+
+path = "Chevrolet-Cruze.csv"
+
+factory = PageFactory()
+factory.create_page(path)
